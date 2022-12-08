3313e3869cdb0e8051a6223f8cc9b93b17ae4769
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-08 22:55:12 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Array from "./modules/Array";
 import ResetButton from "./modules/Controls/ResetButton.tsx/ResetButton";
+import styles from "./App.module.scss";
 
 const App = () => {
   return (
-    <>
+    <div className={styles.app}>
       <ResetButton />
       <Array />
-    </>
+    </div>
   );
 };
 
