9ae6d96e2024c88adbe6a09e374c2ed8ee32a4bc
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-08 22:55:10 +0500

    Update ResetButton.tsx

--- src/modules/Controls/ResetButton.tsx/ResetButton.tsx
+++ src/modules/Controls/ResetButton.tsx/ResetButton.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import { Button } from "../../../shared/components/Button";
-import { useAppDispatch } from "../../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { setArray } from "../../../store/reducers/arraySettings";
 import { createArray } from "../../../utils";
 
 const ResetButton = () => {
   const dispatch = useAppDispatch();
+  const arrayLength = useAppSelector(
+    (state) => state.arraySettings.arrayLength
+  );
 
-  const onClick = () => dispatch(setArray(createArray(30)));
+  const onClick = () => dispatch(setArray(createArray(arrayLength)));
 
   return <Button text="Пересоздать массив" onClick={onClick} />;
 };
