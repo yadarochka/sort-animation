0246f6e619d3f308e9ab1c6cb8fc71227ea04142
yadarochka <66113821+yadarochka@users.noreply.github.com>
2022-12-08 22:55:24 +0500

    Update Array.tsx

--- src/modules/Array/Array.tsx
+++ src/modules/Array/Array.tsx
@@ -2,18 +2,16 @@ import React from "react";
 
 import Bar from "./Bar/Bar";
 import styles from "./Array.module.scss";
-import { createArray } from "../../utils";
 import { useAppSelector } from "../../store/hooks";
 
 const WIDTH_MULTIPLIER = window.screen.width;
-const ARRAY = createArray(30);
-const BAR_WIDTH = WIDTH_MULTIPLIER / ARRAY.length;
 
 const Array = () => {
-  const arrayBars = useAppSelector((state) => state.arraySettings.array);
+  const arrayState = useAppSelector((state) => state.arraySettings);
+  const BAR_WIDTH = WIDTH_MULTIPLIER / arrayState.arrayLength;
   return (
     <div className={styles.array}>
-      {arrayBars.map((height, index) => (
+      {arrayState.array.map((height, index) => (
         <Bar key={index} width={BAR_WIDTH} height={height} />
       ))}
     </div>
